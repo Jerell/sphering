ca894c49e2371d8624cda6f83b96070df7546aa4
Jerell <jerell1998@gmail.com>
2021-02-09 11:20:45 +0000

    broken link

--- components/layout.js
+++ components/layout.js
@@ -12,7 +12,7 @@ export default function Layout({ children, pagesData }) {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Sphering demo" />
-        <meta property="og:image" content="./Pace_Brand.png" />
+        <meta property="og:image" content="/Pace_Brand.png" />
         <meta name="og:title" content={title} />
         <meta name="twitter:card" content="summary-large_image" />
         <title>{title}</title>
