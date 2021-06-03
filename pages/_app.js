b164223e9a0ce71aaac53364305f1abe61569e4b
Jerell <jerell1998@gmail.com>
2021-06-03 14:44:56 +0100

    no provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import "../styles/global.scss";
-import { Provider } from "next-auth/client";
 
 Date.prototype.addDays = function (days) {
   let date = new Date(this.valueOf());
@@ -17,13 +16,11 @@ Date.prototype.end = function (day = 5) {
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider session={pageProps.session}>
-      <div className="flex flex-col min-h-screen">
-        <Component {...pageProps} />
-        <footer className="bg-gray-100 w-full text-center p-4">
-          <p>&copy; Pace Flow Assurance</p>
-        </footer>
-      </div>
-    </Provider>
+    <div className="flex flex-col min-h-screen">
+      <Component {...pageProps} />
+      <footer className="bg-gray-100 w-full text-center p-4">
+        <p>&copy; Pace Flow Assurance</p>
+      </footer>
+    </div>
   );
 }
